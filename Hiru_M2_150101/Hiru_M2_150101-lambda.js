f9ae0e51680be9e8a9fc3cdffa69e94411c92cd4
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-15 15:23:19 +0530

    test commit

--- Hiru_M2_150101/Hiru_M2_150101-lambda.js
+++ Hiru_M2_150101/Hiru_M2_150101-lambda.js
@@ -1,12 +1,40 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
+	console.log('Started the lambda');
 
+	sns.publish({
+		Message: 'This is a test message',
+		Subject: 'this is a test',
+		MessageAttributes: {
+			'key01': {
+				DataType: 'String',
+				StringValue: 'val01'
+			},
+			'key02': {
+				DataType: 'Number',
+				StringValue: '001'
+			},
+			'key03': {
+				DataType: 'Binary',
+				StringValue: 'false'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:hiru_topic'
+	}).promise()
+		.then(data => {
+			// your code goes here
+			console.log('Data', data);
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log('An error occured');
+		});
 
-	
-	callback(null,'Successfully executed');
-
-	console.log('Successfully executed');
+		console.log('End the process');
+		callback(null, 'Successfully executed');
 
 
 }
